1f32bb5d468550ade49419c9be6f0c15bb1d20de
srijanibanerje <srijaniofficial2022@gmail.com>
2026-02-28 16:49:08 +0530

    Update dashboard logic

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettiesweetalertr */
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom'
 import swal from 'sweetalert'
 
 import {
@@ -20,156 +20,187 @@ import {
 
 import WidgetsDropdown from '../widgets/WidgetsDropdown'
 import MainChart from './MainChart'
-import Charts from "../charts/Charts"
+import Charts from '../charts/Charts'
 
 const Dashboard = () => {
- const ROOT_URL = import.meta.env.VITE_LOCALHOST_URL;
- const [courseCount, setCourseCount] = useState({});
-useEffect(() => {
-  const fetchUserPurchases = async () => {
-    try {
-      const res = await axios.get(`${ROOT_URL}/api/users/all`);
+  const ROOT_URL = import.meta.env.VITE_LOCALHOST_URL
+  const [courseCount, setCourseCount] = useState({})
+  useEffect(() => {
+    const fetchUserPurchases = async () => {
+      try {
+        const res = await axios.get(`${ROOT_URL}/api/users/all`)
 
-      const users = res.data.data; // API returns array directly
+        const users = res.data.data // API returns array directly
+        console.log(users)
+        const countMap = {}
 
-      const countMap = {};
+        // users.forEach((user) => {
+        //   const packageName = user?.courseDetails?.packageName;
 
-      users.forEach((user) => {
-        const packageName = user?.courseDetails?.packageName;
+        //   if (packageName) {
+        //     countMap[packageName] = (countMap[packageName] || 0) + 1;
+        //   }
+        // });
 
-        if (packageName) {
-          countMap[packageName] = (countMap[packageName] || 0) + 1;
-        }
-      });
+        users.forEach((user) => {
+          const purchases = user?.courseDetails?.purchaseHistory
 
-      setCourseCount(countMap);
-    } catch (error) {
-      console.error('Error fetching users:', error);
+          if (purchases && purchases.length > 0) {
+            purchases.forEach((purchase) => {
+              const pkg = purchase.packageName
+
+              if (pkg) {
+                countMap[pkg] = (countMap[pkg] || 0) + 1
+              }
+            })
+          }
+        })
+
+        setCourseCount(countMap)
+
+        setCourseCount(countMap)
+      } catch (error) {
+        console.error('Error fetching users:', error)
+      }
     }
-  };
 
-  fetchUserPurchases();
-}, []);
+    fetchUserPurchases()
+  }, [])
 
-  
   // console.log(ROOT_URL)
 
-//   useEffect(() => {
-//     axios.get(ROOT_URL+'/api/v1/get_course')
-    
-//     .then((productdata) =>{
-//       setproductdata(productdata.data.data);
-//       console.log(productdata.data.data);
-//     } )
-//     .catch((err) =>{
-//        console.log(err)
-        
-//      }
-//     )
-     
-// }, []);
+  //   useEffect(() => {
+  //     axios.get(ROOT_URL+'/api/v1/get_course')
 
+  //     .then((productdata) =>{
+  //       setproductdata(productdata.data.data);
+  //       console.log(productdata.data.data);
+  //     } )
+  //     .catch((err) =>{
+  //        console.log(err)
 
+  //      }
+  //     )
 
-// const handleDelete = async (id) => {
-//   try {
-//     // const id = document.getElementById('courseid').innerHTML;
-//     // console.log(id);
-//     await axios.delete(ROOT_URL+`/api/v1/deletecourse/${id}`);
-//     swal("Deleted!", "Course has been deleted.", "success");
-//     window.location.reload();
-//   } catch (error) {
-//     swal("Error!", "Failed to delete the course.", "error");
-//     console.error('Error deleting course', error);
-//   }
-// };
-// const confirmDelete = (productid) => {
-//   console.log("course ID to be deleted:", productid); 
-  
-//   swal({
-//     title: "Are you sure?",
-//     text: "Once deleted, you will not be able to recover this course!",
-//     icon: "warning",
-//     buttons: true,
-//     dangerMode: true,
-//   }).then((willDelete) => {
-//     if (willDelete) {
-//       handleDelete(productid);
-//     } else {
-//       swal("Your course is safe!");
-//     }
-//   });
-// };
+  // }, []);
+
+  // const handleDelete = async (id) => {
+  //   try {
+  //     // const id = document.getElementById('courseid').innerHTML;
+  //     // console.log(id);
+  //     await axios.delete(ROOT_URL+`/api/v1/deletecourse/${id}`);
+  //     swal("Deleted!", "Course has been deleted.", "success");
+  //     window.location.reload();
+  //   } catch (error) {
+  //     swal("Error!", "Failed to delete the course.", "error");
+  //     console.error('Error deleting course', error);
+  //   }
+  // };
+  // const confirmDelete = (productid) => {
+  //   console.log("course ID to be deleted:", productid);
+
+  //   swal({
+  //     title: "Are you sure?",
+  //     text: "Once deleted, you will not be able to recover this course!",
+  //     icon: "warning",
+  //     buttons: true,
+  //     dangerMode: true,
+  //   }).then((willDelete) => {
+  //     if (willDelete) {
+  //       handleDelete(productid);
+  //     } else {
+  //       swal("Your course is safe!");
+  //     }
+  //   });
+  // };
 
   return (
     <>
       <WidgetsDropdown className="mb-4" />
-      <Charts/>
-        <CCardHeader>
-          <h5 className='text-center mb-2'>All Packages</h5>
-        </CCardHeader>
-        <CCardBody>
-          <CTable responsive="sm" color="dark">
-            <CTableHead>
-              <CTableRow>
-               
-                <CTableHeaderCell scope="col"  className='text-center'>Course name</CTableHeaderCell>
-                 <CTableHeaderCell scope="col"  className='text-center'>GST</CTableHeaderCell>
-                 <CTableHeaderCell scope="col"  className='text-center'>Actual Price</CTableHeaderCell>
-                <CTableHeaderCell scope="col"  className='text-center'>Course price</CTableHeaderCell>
-                <CTableHeaderCell scope="col" className='text-center'>Purchased Users</CTableHeaderCell>
+      <Charts />
+      <CCardHeader>
+        <h5 className="text-center mb-2">All Packages</h5>
+      </CCardHeader>
+      <CCardBody>
+        <CTable responsive="sm" color="dark">
+          <CTableHead>
+            <CTableRow>
+              <CTableHeaderCell scope="col" className="text-center">
+                Course name
+              </CTableHeaderCell>
+              <CTableHeaderCell scope="col" className="text-center">
+                GST
+              </CTableHeaderCell>
+              <CTableHeaderCell scope="col" className="text-center">
+                Actual Price
+              </CTableHeaderCell>
+              <CTableHeaderCell scope="col" className="text-center">
+                Course price
+              </CTableHeaderCell>
+              <CTableHeaderCell scope="col" className="text-center">
+                Purchased Users
+              </CTableHeaderCell>
+            </CTableRow>
+          </CTableHead>
+          <CTableBody>
+            <CTableRow active>
+              <CTableDataCell className="text-center">Learner Course</CTableDataCell>
 
-                
-             </CTableRow>
-            </CTableHead>
-            <CTableBody>
-            <CTableRow active >
-                <CTableDataCell  className='text-center'>Learner Course</CTableDataCell>
-                
-                <CTableDataCell  className='text-center'>270/-</CTableDataCell>
-                <CTableDataCell className='text-center'>1500/-</CTableDataCell>
+              <CTableDataCell className="text-center">270/-</CTableDataCell>
+              <CTableDataCell className="text-center">1500/-</CTableDataCell>
 
-                <CTableDataCell className='text-center'>1770/-</CTableDataCell>
-                <CTableDataCell className='text-center'>{courseCount['Learner Course'] || 0}</CTableDataCell>
+              <CTableDataCell className="text-center">1770/-</CTableDataCell>
+              <CTableDataCell className="text-center">
+                {courseCount['Learner Course'] || 0}
+              </CTableDataCell>
+            </CTableRow>
+            <CTableRow>
+              <CTableDataCell className="text-center">Master Course</CTableDataCell>
+              <CTableDataCell className="text-center">540/-</CTableDataCell>
+              <CTableDataCell className="text-center">3000/-</CTableDataCell>
+              <CTableDataCell className="text-center">3540/-</CTableDataCell>
+              <CTableDataCell className="text-center">
+                {courseCount['Master Course'] || 0}
+              </CTableDataCell>
+            </CTableRow>
+            <CTableRow>
+              <CTableDataCell className="text-center">Pro Master Course</CTableDataCell>
+              <CTableDataCell className="text-center">1080/-</CTableDataCell>
+              <CTableDataCell className="text-center">6000/-</CTableDataCell>
+              <CTableDataCell className="text-center">7080/-</CTableDataCell>
+              <CTableDataCell className="text-center">
+                {courseCount['Pro Master Course'] || 0}
+              </CTableDataCell>
+            </CTableRow>
+            <CTableRow>
+              <CTableDataCell className="text-center">Teacher Course</CTableDataCell>
+              <CTableDataCell className="text-center">1800/-</CTableDataCell>
+              <CTableDataCell className="text-center">10000/-</CTableDataCell>
 
+              <CTableDataCell className="text-center">11800/-</CTableDataCell>
+              <CTableDataCell className="text-center">
+                {courseCount['Teacher Course'] || 0}
+              </CTableDataCell>
             </CTableRow>
-            <CTableRow >
-                <CTableDataCell className='text-center'>Master Course</CTableDataCell>
-                 <CTableDataCell className='text-center'>540/-</CTableDataCell>
-                  <CTableDataCell className='text-center'>3000/-</CTableDataCell>
-                <CTableDataCell className='text-center'>3540/-</CTableDataCell>
-                <CTableDataCell className='text-center'>{courseCount['Master Course'] || 0}</CTableDataCell>
+            <CTableRow>
+              <CTableDataCell className="text-center">Pro Teacher Course</CTableDataCell>
+              <CTableDataCell className="text-center">9000/-</CTableDataCell>
+              <CTableDataCell className="text-center">50000/-</CTableDataCell>
+              <CTableDataCell className="text-center">59000/-</CTableDataCell>
+              <CTableDataCell className="text-center">
+                {courseCount['Pro Teacher Course'] || 0}
+              </CTableDataCell>
             </CTableRow>
-            <CTableRow >
-                <CTableDataCell className='text-center'>Pro Master Course</CTableDataCell>
-                <CTableDataCell className='text-center'>1080/-</CTableDataCell>
-                 <CTableDataCell className='text-center'>6000/-</CTableDataCell>
-                <CTableDataCell className='text-center'>7080/-</CTableDataCell>
-                <CTableDataCell className='text-center'>{courseCount['Pro Master Course'] || 0}</CTableDataCell>
+            <CTableRow>
+              <CTableDataCell className="text-center">Monthly Subscription</CTableDataCell>
+              <CTableDataCell className="text-center">144/-</CTableDataCell>
+              <CTableDataCell className="text-center">800/-</CTableDataCell>
+              <CTableDataCell className="text-center">944/-</CTableDataCell>
+              <CTableDataCell className="text-center">
+                {courseCount['Monthly Subscription'] || 0}
+              </CTableDataCell>
             </CTableRow>
-            <CTableRow >
-                <CTableDataCell className='text-center'>Teacher Course</CTableDataCell>
-                <CTableDataCell className='text-center'>1800/-</CTableDataCell>
-                 <CTableDataCell className='text-center'>10000/-</CTableDataCell>
-
-                <CTableDataCell className='text-center'>11800/-</CTableDataCell>
-                <CTableDataCell className='text-center'>{courseCount['Teacher Course'] || 0}</CTableDataCell>
-            </CTableRow>
-            <CTableRow >
-                <CTableDataCell className='text-center'>Pro Teacher Course</CTableDataCell>
-                <CTableDataCell className='text-center'>9000/-</CTableDataCell>
-                  <CTableDataCell className='text-center'>50000/-</CTableDataCell>
-                <CTableDataCell className='text-center'>59000/-</CTableDataCell>
-                <CTableDataCell className='text-center'>{courseCount['Pro Teacher Course'] || 0}</CTableDataCell>
-            </CTableRow>
-            <CTableRow >
-                <CTableDataCell className='text-center'>Monthly Subscription</CTableDataCell>
-                <CTableDataCell className='text-center'>144/-</CTableDataCell>
-                 <CTableDataCell className='text-center'>800/-</CTableDataCell>
-                <CTableDataCell className='text-center'>944/-</CTableDataCell>
-                <CTableDataCell className='text-center'>{courseCount['Monthly Subscription'] || 0}</CTableDataCell>
-            </CTableRow>
-             {/* {
+            {/* {
               productdata.map((product) => {
                 return <CTableRow active key={product._id} >
                   <CTableDataCell id='courseid'>{product._id}</CTableDataCell>
@@ -193,11 +224,10 @@ useEffect(() => {
                 </CTableRow>
               })
              } */}
-            </CTableBody>
-          </CTable>
-        </CCardBody>
-     
-    
+          </CTableBody>
+        </CTable>
+      </CCardBody>
+
       <CCardFooter></CCardFooter>
     </>
   )
